b0683b0988227d82c978fab81289637f282bcab9
Kieran Kilburn-Phillips <kieran.kilburnphillips@vivedia.net>
2026-01-22 12:23:48 +0000

    Protect all canvassing routes with authentication middleware

--- routes/web.php
+++ routes/web.php
@@ -4,28 +4,44 @@ use App\Http\Controllers\AddressImportController;
 use App\Http\Controllers\CanvasserController;
 use App\Http\Controllers\CanvassingController;
 use App\Http\Controllers\ExportController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [CanvassingController::class, 'index'])->name('canvassing.index');
+Route::get('/', function () {
+    return redirect()->route('canvassing.index');
+});
 
-// Canvassing routes
-Route::get('/street/{streetName}', [CanvassingController::class, 'street'])->name('canvassing.street');
-Route::post('/knock-result', [CanvassingController::class, 'store'])->name('knock-result.store');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Canvasser management routes
-Route::get('/canvassers', [CanvasserController::class, 'index'])->name('canvassers.index');
-Route::post('/canvassers', [CanvasserController::class, 'store'])->name('canvassers.store');
-Route::delete('/canvassers/{canvasser}', [CanvasserController::class, 'destroy'])->name('canvassers.destroy');
-Route::patch('/canvassers/{canvasser}/toggle', [CanvasserController::class, 'toggleActive'])->name('canvassers.toggle');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-// Export routes
-Route::get('/exports', [ExportController::class, 'index'])->name('exports.index');
-Route::get('/exports/create', [ExportController::class, 'create'])->name('exports.create');
-Route::post('/exports', [ExportController::class, 'store'])->name('exports.store');
-Route::get('/exports/{export}/download', [ExportController::class, 'download'])->name('exports.download');
-Route::delete('/exports/{export}', [ExportController::class, 'destroy'])->name('exports.destroy');
+    // Canvassing routes
+    Route::get('/canvassing', [CanvassingController::class, 'index'])->name('canvassing.index');
+    Route::get('/street/{streetName}', [CanvassingController::class, 'street'])->name('canvassing.street');
+    Route::post('/knock-result', [CanvassingController::class, 'store'])->name('knock-result.store');
 
-// Import routes
-Route::get('/import', [AddressImportController::class, 'index'])->name('import.index');
-Route::post('/import', [AddressImportController::class, 'store'])->name('import.store');
-Route::delete('/import/clear', [AddressImportController::class, 'clear'])->name('import.clear');
+    // Canvasser management routes
+    Route::get('/canvassers', [CanvasserController::class, 'index'])->name('canvassers.index');
+    Route::post('/canvassers', [CanvasserController::class, 'store'])->name('canvassers.store');
+    Route::delete('/canvassers/{canvasser}', [CanvasserController::class, 'destroy'])->name('canvassers.destroy');
+    Route::patch('/canvassers/{canvasser}/toggle', [CanvasserController::class, 'toggleActive'])->name('canvassers.toggle');
+
+    // Export routes
+    Route::get('/exports', [ExportController::class, 'index'])->name('exports.index');
+    Route::get('/exports/create', [ExportController::class, 'create'])->name('exports.create');
+    Route::post('/exports', [ExportController::class, 'store'])->name('exports.store');
+    Route::get('/exports/{export}/download', [ExportController::class, 'download'])->name('exports.download');
+    Route::delete('/exports/{export}', [ExportController::class, 'destroy'])->name('exports.destroy');
+
+    // Import routes
+    Route::get('/import', [AddressImportController::class, 'index'])->name('import.index');
+    Route::post('/import', [AddressImportController::class, 'store'])->name('import.store');
+    Route::delete('/import/clear', [AddressImportController::class, 'clear'])->name('import.clear');
+});
+
+require __DIR__.'/auth.php';
