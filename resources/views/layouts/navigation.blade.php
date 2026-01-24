7443fadb55a2d033214eaf4f3358ca9e4464994c
Kieran Kilburn-Phillips <kieran.kilburnphillips@vivedia.net>
2026-01-24 22:41:29 +0000

    Update mobile nav styling to be more in line

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -99,10 +99,10 @@
         </div>
 
         <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
+        <div class="pt-4 pb-1 border-t border-white border-opacity-20">
             <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                <div class="font-medium text-base text-white">{{ Auth::user()->name }}</div>
+                <div class="font-medium text-sm text-white text-opacity-80">{{ Auth::user()->email }}</div>
             </div>
 
             <div class="mt-3 space-y-1">
