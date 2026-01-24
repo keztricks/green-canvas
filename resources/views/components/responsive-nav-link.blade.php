fa9fff2e1688976d998d542aae86cce2394f9c49
Kieran Kilburn-Phillips <kieran.kilburnphillips@vivedia.net>
2026-01-24 22:41:29 +0000

    Update mobile nav styling to be more in line

--- resources/views/components/responsive-nav-link.blade.php
+++ resources/views/components/responsive-nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'block w-full ps-3 pe-4 py-2 border-l-4 border-[#6AB023] text-start text-base font-medium text-[#5a9620] bg-green-50 focus:outline-none focus:text-[#4a8610] focus:bg-green-100 focus:border-[#5a9620] transition duration-150 ease-in-out'
-            : 'block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out';
+            ? 'block w-full ps-3 pe-4 py-2 border-l-4 border-white text-start text-base font-medium text-[#6AB023] bg-white focus:outline-none focus:text-[#5a9620] focus:bg-white focus:border-white transition duration-150 ease-in-out'
+            : 'block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-white hover:text-gray-200 hover:bg-[#5a9620] focus:outline-none focus:text-gray-200 focus:bg-[#5a9620] transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
