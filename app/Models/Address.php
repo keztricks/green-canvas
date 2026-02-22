5633a09833bf8eb0420a77f756d4755e7e923f1e
Kieran Kilburn-Phillips <kieran.kilburnphillips@vivedia.net>
2026-02-22 19:43:27 +0000

    Update election filters for at least one match

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -69,21 +69,57 @@ class Address extends Model
             return $query;
         }
 
-        // ANY logic: address must match AT LEAST ONE selected election
-        // For each election, ANY of its selected statuses can match
+        // ANY logic: address must match AT LEAST ONE selected election/status combination
         $query->where(function($q) use ($electionFilters) {
             foreach ($electionFilters as $electionId => $statuses) {
                 if (empty($statuses)) {
                     continue;
                 }
                 
-                $q->orWhereHas('elections', function ($subQ) use ($electionId, $statuses) {
-                    $subQ->where('election_id', $electionId)
-                         ->whereIn('address_election.status', $statuses);
+                // Check if 'unknown' is in the selected statuses
+                $hasUnknown = in_array('unknown', $statuses);
+                $otherStatuses = array_diff($statuses, ['unknown']);
+                
+                // Build OR condition for this election
+                $q->orWhere(function($subQ) use ($electionId, $hasUnknown, $otherStatuses) {
+                    // If there are non-unknown statuses, check for addresses with those statuses
+                    if (!empty($otherStatuses)) {
+                        $subQ->whereHas('elections', function ($electionQ) use ($electionId, $otherStatuses) {
+                            $electionQ->where('election_id', $electionId)
+                                     ->whereIn('address_election.status', $otherStatuses);
+                        });
+                    }
+                    
+                    // If 'unknown' is selected, also include addresses with status='unknown' 
+                    // OR addresses that don't have any record for this election
+                    if ($hasUnknown) {
+                        if (!empty($otherStatuses)) {
+                            // If we already added other statuses, use OR
+                            $subQ->orWhereHas('elections', function ($electionQ) use ($electionId) {
+                                $electionQ->where('election_id', $electionId)
+                                         ->where('address_election.status', 'unknown');
+                            });
+                            $subQ->orWhereDoesntHave('elections', function ($electionQ) use ($electionId) {
+                                $electionQ->where('election_id', $electionId);
+                            });
+                        } else {
+                            // Only unknown is selected, so we need whereHas OR doesntHave
+                            $subQ->where(function($unknownQ) use ($electionId) {
+                                $unknownQ->whereHas('elections', function ($electionQ) use ($electionId) {
+                                    $electionQ->where('election_id', $electionId)
+                                             ->where('address_election.status', 'unknown');
+                                })
+                                ->orWhereDoesntHave('elections', function ($electionQ) use ($electionId) {
+                                    $electionQ->where('election_id', $electionId);
+                                });
+                            });
+                        }
+                    }
                 });
             }
         });
 
         return $query;
     }
+
 }
